80f1ed46f01ea3560e9e3fdd268918740243fabd
eggplaan <eggplaan@gmail.com>
2022-03-01 16:59:35 -0500

    add conditions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // you can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -24,7 +25,7 @@ var fight = function() {
 				playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
 				);
 			
-				// check enemy's health
+			// check enemy's health
 				if (enemyHealth <= 0) {
 					window.alert(enemyName + " has died!");
 				} 
@@ -49,11 +50,25 @@ var fight = function() {
 		// if player chooses to skip
 		} 
 		else if (promptFight === "skip" || promptFight === "SKIP") {
+			// confirm player wants to skip
+			var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+				// if yes (true), then leave fight
+				if (confirmSkip) {
+					window.alert(playerName + " has decided to skip this fight.  Goodbye!");
+					// subtract money from playerMoney for skipping
+					playerMoney = playerMoney - 2;
+				}
+
+				// if no (false), then ask question again by running fight () again
+				else {
+					fight();
+				}
+		}	
 			window.alert(playerName + " has chosen to skip the fight!");
 		} 
 		else {
 			window.alert("You need to choose a valid option. Try again!");
 		}
-	}
 		
 fight();
